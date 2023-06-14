1281a3823386d0b6601065112d967fd1c84b4144
SevicheCC <91365763+Sevichecc@users.noreply.github.com>
2023-06-15 02:33:54 +0800

    feat: detail page

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,10 +1,9 @@
 import { useFetch } from "@raycast/utils";
 import { useState } from "react";
 import { SearchData } from "../types";
+
 const useSearch = () => {
   const [searchText, setSearchText] = useState("");
-  // const [searchCategory, setSerachCategory] = useState("");
-
   const { isLoading, data, error } = useFetch<SearchData>(
     `https://neodb.social/api/catalog/search?query=${searchText}'`,
     {
--- src/search.tsx
+++ src/search.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from "react";
-import { Grid } from "@raycast/api";
+import { Action, ActionPanel, Grid, Icon } from "@raycast/api";
 import { Category, Item } from "./types";
+import ItemDetail from "./ItemDetail";
 import useSearch from "./hooks/useSearch";
 
 interface SectionProps {
@@ -11,6 +12,13 @@ interface SectionProps {
 
 const titleParser = (title: string) => (title === "tv" ? "TV" : `${title.charAt(0).toUpperCase() + title.slice(1)}`);
 
+const categories: Category[] = ["book", "movie", "tv", "music", "game", "podcast", "performance"];
+
+const aspectRatios: Partial<Record<Category, SectionProps["aspectRatio"]>> = {
+  music: "1",
+  podcast: "1",
+};
+
 const Section: React.FC<SectionProps> = ({ items, category, aspectRatio }) => {
   return (
     <Grid.Section title={titleParser(category)} fit={Grid.Fit.Fill} aspectRatio={aspectRatio ?? "2/3"}>
@@ -20,6 +28,12 @@ const Section: React.FC<SectionProps> = ({ items, category, aspectRatio }) => {
           content={{ value: { source: item.cover_image_url }, tooltip: item.brief }}
           title={item.display_title}
           subtitle={item.brief}
+          actions={
+            <ActionPanel>
+              <Action.Push title="View Detail" target={<ItemDetail item={item} />} icon={Icon.Glasses} />
+              <Action.OpenInBrowser url={`https://neodb.social${item.url}`} title="Open in Browser" />
+            </ActionPanel>
+          }
         />
       ))}
     </Grid.Section>
@@ -47,26 +61,8 @@ export default function Command() {
     return itemsByCategory;
   }, [searchData]);
 
-  const categories: Category[] = ["book", "movie", "tv", "music", "game", "podcast", "performance"];
-  const aspectRatios: Partial<Record<Category, SectionProps["aspectRatio"]>> = {
-    music: "1",
-    podcast: "1",
-  };
-
   return (
-    <Grid
-      throttle
-      columns={7}
-      isLoading={isLoading}
-      onSearchTextChange={setSearchText}
-      searchBarAccessory={
-        <Grid.Dropdown tooltip="Dropdown With Items">
-          {categories.map((category) => (
-            <Grid.Dropdown.Item key={category} title={titleParser(category)} value={category} />
-          ))}
-        </Grid.Dropdown>
-      }
-    >
+    <Grid throttle columns={7} isLoading={isLoading} onSearchTextChange={setSearchText}>
       {categories.map((category) => (
         <Section
           key={category}
